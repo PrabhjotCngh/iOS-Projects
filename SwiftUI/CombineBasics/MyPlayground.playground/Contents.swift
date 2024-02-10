f019d688dd2cd5fe9b72239b7bb24bb9ee280e02
Prabjot Singh <prabjot.singh@andersencorp.com>
2024-02-10 13:50:19 -0500

    Use of subjects in Combine

--- SwiftUI/CombineBasics/MyPlayground.playground/Contents.swift
+++ SwiftUI/CombineBasics/MyPlayground.playground/Contents.swift
@@ -368,5 +368,102 @@ publisher.send(8)
 */
 
 
+// Subjects - publish and they can also subscribe
 
+// PassthroughSubject
 
+/*
+ let subject = PassthroughSubject<Int, Never>()
+ 
+ let cancellable21 = subject.sink { value in
+ print(value)
+ }
+ 
+ 
+ subject.send(1)
+ subject.send(2)
+ 
+ */
+
+// CurrentValueSubject
+
+/*
+let currentValueSubject = CurrentValueSubject<String, Never>("John")
+
+let cancellable22 = currentValueSubject.sink { value in
+    print(value)
+}
+
+currentValueSubject.send("Mary")
+
+print(currentValueSubject.value)
+
+*/
+
+// Custom subjects
+
+/*
+class EvenSubject<Failure: Error>: Subject {
+    
+    typealias Output = Int
+    
+    private let wrapped: PassthroughSubject<Int, Failure>
+    
+    init(initialValue: Int) {
+        self.wrapped = PassthroughSubject()
+        let evenInitialValue = initialValue % 2 == 0 ? initialValue: 0
+        send(initialValue)
+    }
+    
+    func send(subscription: Subscription) {
+        wrapped.send(subscription: subscription)
+    }
+    
+    func send(_ value: Int) {
+        if value % 2 == 0 {
+            wrapped.send(value)
+        }
+    }
+    
+    func send(completion: Subscribers.Completion<Failure>) {
+        wrapped.send(completion: completion)
+    }
+    
+    func receive<S>(subscriber: S) where S : Subscriber, Failure == S.Failure, Int == S.Input {
+        wrapped.receive(subscriber: subscriber)
+    }
+    
+}
+
+let subject = EvenSubject<Never>(initialValue: 4)
+
+let cancellable = subject.sink { value in
+    print(value)
+}
+
+subject.send(12)
+subject.send(13)
+subject.send(20)
+
+*/
+
+// WeatherClient using subjects
+
+/*
+class WeatherClient {
+    let updates = PassthroughSubject<Int, Never>()
+    
+    func fetchWeather() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) { [weak self] in
+            self?.updates.send(Int.random(in: 0...100))
+        }
+    }
+}
+
+let client = WeatherClient()
+let cancellable23 = client.updates.sink { value in
+    print(value)
+}
+
+client.fetchWeather()
+*/
