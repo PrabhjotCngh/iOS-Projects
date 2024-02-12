35ac8a4bff92e77544bf2a765b9e0da57ac6905a
Prabjot Singh <prabjot.singh@andersencorp.com>
2024-02-11 19:55:43 -0500

    Completed the API integration with combine using SwiftUI

--- SwiftUI/SwiftUIMovieApp/SwiftUIMovieApp/ContentView.swift
+++ SwiftUI/SwiftUIMovieApp/SwiftUIMovieApp/ContentView.swift
@@ -6,19 +6,76 @@
 //
 
 import SwiftUI
+import Combine
 
 struct ContentView: View {
+    @State private var movies: [Movie] = []
+    @State private var search: String = ""
+    
+    private var service: HTTPClientService
+    
+    private var searchSubject = CurrentValueSubject<String, Never>("")
+    @State private var cancellables: Set<AnyCancellable> = []
+
+    init(service: HTTPClientService) {
+        self.service = service
+    }
+    
+    //MARK: - Private functions
+    private func setupSearchPublisher() {
+        searchSubject
+            .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+            .sink { searchText in
+                loadMovies(search: searchText)
+            }.store(in: &cancellables)
+    }
+    
+    private func loadMovies(search: String) {
+        service.fetchMovies(searchString: search).sink { completion in
+
+            switch completion {
+            case .finished:
+                print("Finished")
+            case .failure(let error):
+                print(error)
+            }
+        } receiveValue: { movies in
+            self.movies = movies
+        }.store(in: &cancellables)
+
+    }
+    
+    //MARK: - Body
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List(movies) { movie in
+            HStack {
+                AsyncImage(url: movie.poster) { image in
+                    image.resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 75, height: 75)
+                } placeholder: {
+                    ProgressView()
+                }
+                Text(movie.title)
+            }
         }
-        .padding()
+        .onAppear {
+            setupSearchPublisher()
+        }
+        .searchable(text: $search)
+        .onChange(of: search) {
+            searchSubject.send(search)
+        }
+        .overlay {
+            if movies.isEmpty {
+                ContentUnavailableView.search(text: search)
+            }
+        }
     }
 }
 
 #Preview {
-    ContentView()
+    NavigationStack {
+        ContentView(service: HTTPClientFactory.create())
+    }
 }
